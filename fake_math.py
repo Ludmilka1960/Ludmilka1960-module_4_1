2bc59cd56d8da303f702af2dc6703e50659de351
Ludmilka1960.ru <liudmilka2303@gmail.com>
2024-11-17 17:45:22 +0200

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,16 @@
+
+
+
+def divide(first, sekond):
+    result= (first, sekond)
+    if sekond ==(0):
+        return 'ошибка'
+
+
+    else:
+        return result
+
+result1 =  divide(12, 4)
+result2 =  divide(12, 0)
+print(result1)
+print(result2)
