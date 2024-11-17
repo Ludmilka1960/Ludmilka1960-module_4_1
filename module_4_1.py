d1c7fc11ba0f262d6c034d555e6af375e0c076d9
Ludmilka1960.ru <liudmilka2303@gmail.com>
2024-11-17 17:43:26 +0200

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as fm_div
+from true_math import divide as tm_div
+print('result1 = ', fm_div(12, 4))
+print('result2 = ', fm_div(12, 0))
+print('result3 = ', tm_div(13, 5))
+print('result4 = ', tm_div(13, 0))
